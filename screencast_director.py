bad6e7cefaf3617457b78ac2b879603836b50123
Colin T.A. Gray <colinta@gmail.com>
2013-04-10 07:31:03 -0600

    fixes and improvements

--- screencast_director.py
+++ screencast_director.py
@@ -2,6 +2,15 @@ import random
 import sublime
 import sublime_plugin
 import yaml
+from functools import reduce
+
+
+def parse(str, check_nl=True):
+    if check_nl and "\n" in str:
+        return "\n".join(map(lambda line: parse(line, False), str.split("\n")))
+    if len(str) > 1 and str[0] == '"' and str[-1] == '"':
+        str = eval(str)
+    return str
 
 
 class ScreencastDirector(object):
@@ -16,7 +25,6 @@ class ScreencastDirector(object):
         if self.source_view is None:
             return
 
-        window = self.source_view.window()
         active_view = sublime.active_window().active_view()
         regions = self.source_view.get_regions('screencast_director')
         if self.index < 0:
@@ -32,6 +40,13 @@ class ScreencastDirector(object):
         if abs(new_pos[0] - pos[0]) <= 1.0 and abs(new_pos[1] - pos[1]) <= 1.0:
             self.source_view.set_viewport_position((new_pos[0], new_pos[1] + 1))
             self.source_view.set_viewport_position((new_pos[0], new_pos[1]))
+        window = self.source_view.window()
+        if not window:
+            window = self.target_view.window()
+        if not window:
+            sublime.status_message('I can\'t find a `window`')
+            return
+
         window.focus_view(self.source_view)
         window.focus_view(active_view)
 
@@ -42,7 +57,10 @@ class ScreencastDirector(object):
         commands = yaml.load(content)
         for entry in commands:
             self._execute(entry)
-        region = self.target_view.sel()[0]
+        if len(self.target_view.sel()):
+            region = self.target_view.sel()[0]
+        else:
+            region = sublime.Region(0, 0)
         self.target_view.add_regions('screencast_director', [region], 'source', '', sublime.HIDDEN)
         self._start_timer()
 
@@ -102,9 +120,9 @@ class ScreencastDirector(object):
             if cursor in self.target_view.sel():
                 self.target_view.sel().subtract(cursor)
 
-            e = self.target_view.begin_edit('screencast_director')
-            new_cursor = cmd(cursor, e)
-            self.target_view.end_edit(e)
+            edit = self.target_view.begin_edit('screencast_director')
+            new_cursor = cmd(cursor, edit)
+            self.target_view.end_edit(edit)
 
             if new_cursor is None:
                 new_cursor = cursor
@@ -117,11 +135,31 @@ class ScreencastDirector(object):
             self.target_view.add_regions('screencast_director', [new_cursor], 'source', '', sublime.HIDDEN)
             sublime.set_timeout(self._start_timer, delay)
 
-    def write(self, *what_to_write):
+    def set_syntax(self, syntax):
+        def _set_syntax(cursor, edit):
+            self.target_view.set_syntax_file(syntax)
+            return cursor
+        self._append_command(_set_syntax)
+
+    def write_at(self, row, col, text):
+        self.goto(row, col)
+        self.write(text)
+
+    def write(self, *what_to_write, **options):
+        if len(what_to_write) == 1 and isinstance(what_to_write[0], dict):
+            return self.write(what_to_write[0]['write'], **what_to_write[0])
+
+        if options.get('write'):
+            what_to_write = options['write']
+            if isinstance(what_to_write, basestring):
+                what_to_write = [what_to_write]
+        delay_min = options.get('delay_min', 40)
+        delay_max = options.get('delay_max', 70)
+
         def _write_letter(letter):
-            def _write(cursor, e):
-                self.target_view.replace(e, cursor, letter)
-                return cursor.a + len(letter)
+            def _write(cursor, edit):
+                self.target_view.replace(edit, cursor, letter)
+                return cursor.begin() + len(letter)
             return _write
 
         if len(what_to_write) > 1:
@@ -136,12 +174,13 @@ class ScreencastDirector(object):
         for entry in what_to_write:
             previous_letter = None
             if isinstance(entry, basestring):
+                entry = parse(entry)
                 for letter in entry:
-                    if previous_letter == letter:
-                        delay = 50
+                    if delay_min == delay_max:
+                        delay = delay_min
+                    elif previous_letter == letter:
+                        delay = delay_min
                     else:
-                        delay_min = 50
-                        delay_max = 100
                         delay = random.randrange(delay_min, delay_max)
                     self._append_command(_write_letter(letter), delay=delay)
                     previous_letter = letter
@@ -175,9 +214,9 @@ class ScreencastDirector(object):
                 right=right, len_right=len(right))
 
         def _write_letters(a, b):
-            def _write(cursor, e):
-                self.target_view.replace(e, cursor, a + b)
-                return cursor.a + len(a)
+            def _write(cursor, edit):
+                self.target_view.replace(edit, cursor, a + b)
+                return cursor.begin() + len(a)
             return _write
 
         index = len(right)
@@ -198,56 +237,181 @@ class ScreencastDirector(object):
             self.go(len(a))
 
     def insert(self, what_to_write, delay=None):
-        def _insert(cursor, e):
-            self.target_view.replace(e, cursor, what_to_write)
-            return cursor.a + len(what_to_write)
+        def _insert(cursor, edit):
+            self.target_view.replace(edit, cursor, what_to_write)
+            return cursor.begin() + len(what_to_write)
         self._append_command(_insert, delay)
 
+    def _write_at(self, edit, row, col, text):
+        # support multiple lines
+        if "\n" in text:
+            lines = text.splitlines()
+            for i, line in enumerate(lines):
+                self._write_at(edit, row + i, col, line)
+            return
+
+        # fix number of rows
+        actual_row, _ = self.target_view.rowcol(self.target_view.text_point(row, 0))
+        nl = max(0, row - actual_row)
+        if nl:
+            point = self.target_view.text_point(actual_row, 0)
+            eol_point = self.target_view.line(point).end()
+            self.target_view.insert(edit, eol_point, "\n" * nl)
+        # fix column
+        line = self.target_view.line(self.target_view.text_point(row, 0))
+        spaces = max(0, col + len(text) - len(line))
+        if spaces:
+            point = line.end()
+            self.target_view.insert(edit, point, ' ' * spaces)
+        # k, we should be good to go now
+        point = self.target_view.text_point(row, col)
+        self.target_view.replace(edit, sublime.Region(point, point + len(text)), text)
+        return point + len(text)
+
+    def write_lines(self, *lines, **options):
+        if len(lines) == 1 and isinstance(lines[0], dict):
+            return self.write_lines(*lines[0]['lines'], **lines[0])
+        delay = options.get('delay', 20)
+
+        def func_maker(char, line_index, char_index):
+            def _write_char(cursor, edit):
+                row, col = self.target_view.rowcol(cursor.begin())
+                line_row = row + line_index
+                line_col = col + char_index
+                self._write_at(edit, line_row, line_col, char)
+                return cursor.begin()
+            return _write_char
+
+        info = {'index': 0}
+        longest_line_len = reduce(lambda a, b: max(a, len(b)), lines, 0)
+        while info['index'] < longest_line_len:
+            for line_index, line in enumerate(lines):
+                try:
+                    char = line[info['index']]
+                    char_index = info['index']
+                    fn = func_maker(char, line_index, char_index)
+                    self._append_command(fn, 0)
+                except IndexError:
+                    pass
+            info['index'] += 1
+            self.delay(delay)
+
     def nl(self, delay=None):
-        def _nl(cursor, e):
-            self.target_view.replace(e, cursor, "\n")
-            return cursor.a + 1
+        def _nl(cursor, edit):
+            self.target_view.replace(edit, cursor, "\n")
+            return cursor.begin() + 1
         self._append_command(_nl, delay)
 
     def delay(self, delay=100):
-        def _delay(cursor, e):
+        def _delay(cursor, edit):
             return cursor
         self._append_command(_delay, delay)
 
     def go(self, where, delay=None):
-        def _go(cursor, e):
-            cursor = cursor.a + where
+        def _go(cursor, edit):
+            cursor = cursor.begin() + where
             self.target_view.sel().clear()
             self.target_view.sel().add(sublime.Region(cursor, cursor))
             return cursor
         self._append_command(_go, delay)
 
     def select_all(self, delay=None):
-        def _select_all(cursor, e):
+        def _select_all(cursor, edit):
             allofit = sublime.Region(0, self.target_view.size())
             self.target_view.sel().clear()
             self.target_view.sel().add(allofit)
             return allofit
         self._append_command(_select_all, delay)
 
+    def select_delta(self, delta, delay=None):
+        def _select_delta(cursor, edit):
+            selection = sublime.Region(cursor.begin(), cursor.end() + delta)
+            self.target_view.sel().clear()
+            self.target_view.sel().add(selection)
+            return selection
+        self._append_command(_select_delta, delay)
+
+    def select_eol(self, delay=None):
+        def _select_eol(cursor, edit):
+            selection = sublime.Region(cursor.begin(), self.target_view.line(cursor.a).end())
+            self.target_view.sel().clear()
+            self.target_view.sel().add(selection)
+            return selection
+        self._append_command(_select_eol, delay)
+
+    def select_next(self, find_next, delay=None):
+        def _select_delta(cursor, edit):
+            selection = self.target_view.find(find_next, cursor.begin(), sublime.LITERAL)
+            self.target_view.sel().clear()
+            self.target_view.sel().add(selection)
+            return selection
+        self._append_command(_select_delta, delay)
+
     def delete(self, delay=None):
-        def _delete(cursor, e):
-            self.target_view.replace(e, cursor, '')
-            return cursor.a
+        def _delete(cursor, edit):
+            self.target_view.replace(edit, cursor, '')
+            return cursor.begin()
         self._append_command(_delete, delay)
 
-    def clear(self):
-        self.select_all()
-        self.delete()
-        self.clear_marks()
+    def clear(self, delay=None):
+        self.select_all(delay)
+        self.delete(delay)
+        self.clear_marks(delay)
+
+    def select_lines(self, line_a, line_b, delay=None):
+        def _select_lines(cursor, edit):
+            last_row = self.target_view.rowcol(len(self.target_view))[0] + 1
+            row_a = line_a
+            row_b = line_b
+            if row_a < 0:
+                row_a = last_row + row_a
+            if row_b < 0:
+                row_b = last_row + row_b
+            point_a = self.target_view.text_point(row_a, 0)
+            point_b = self.target_view.text_point(row_b, 0)
+            # return if the lines are unreachable
+            if self.target_view.rowcol(point_a)[0] != line_a:
+                return cursor.a
+            start = self.target_view.full_line(point_a).begin()
+            stop = self.target_view.full_line(point_b).end()
+            selection = sublime.Region(start, stop)
+            self.target_view.sel().clear()
+            self.target_view.sel().add(selection)
+            return selection
+        self._append_command(_select_lines, delay)
+
+    def clear_lines(self, line_a, line_b, delay=None):
+        self.select_lines(line_a, line_b, delay)
+        self.delete(delay)
+
+    def insert_at(self, row, col, text):
+        def _insert(cursor, edit):
+            return self._write_at(edit, row, col, text)
+        self._append_command(_insert, 0)
+
+    def goto_eol(self):
+        def _goto_eol(cursor, edit):
+            cursor = sublime.Region(self.target_view.line(cursor.a).end())
+            self.target_view.sel().clear()
+            self.target_view.sel().add(cursor)
+            return cursor
+        self._append_command(_goto_eol, 0)
+
+    def goto(self, row, col):
+        self.insert_at(row, col, '')
+
+    def add_cursor(self, row, col):
+        def _add_cursor(cursor, edit):
+            return cursor
+        self.insert_at(row, col, '')
 
     def set_mark(self, name=None, delay=None):
         if not name:
             name = '__tmp__'
 
-        def _set_mark(cursor, e):
-            cursor_0 = self.target_view.line(cursor.a).a
-            self._mark_offsets[name] = cursor.a - cursor_0
+        def _set_mark(cursor, edit):
+            cursor_0 = self.target_view.line(cursor.begin()).a
+            self._mark_offsets[name] = cursor.begin() - cursor_0
             if self._mark_offsets[name] == 0:
                 self._mark_offsets[name] = 1
                 cursor_0 -= 1
@@ -259,7 +423,7 @@ class ScreencastDirector(object):
         if not name:
             name = '__tmp__'
 
-        def _goto_mark(cursor, e):
+        def _goto_mark(cursor, edit):
             cursors = self.target_view.get_regions('screencast_director_%s' % name)
             return cursors[0].a + self._mark_offsets[name]
         self._append_command(_goto_mark, delay)
@@ -271,7 +435,7 @@ class ScreencastDirector(object):
         if not name:
             name = '__tmp__'
 
-        def _select_from_mark(cursor, e):
+        def _select_from_mark(cursor, edit):
             cursors = self.target_view.get_regions('screencast_director_%s' % name)
             a = cursors[0].a + self._mark_offsets[name]
             b = cursor.b
@@ -279,7 +443,7 @@ class ScreencastDirector(object):
         self._append_command(_select_from_mark, delay)
 
     def clear_marks(self, delay=None):
-        def _clear_marks(cursor, e):
+        def _clear_marks(cursor, edit):
             for name in self._mark_offsets:
                 self.target_view.erase_regions('screencast_director_%s' % name)
             self._mark_offsets = {}
@@ -287,7 +451,7 @@ class ScreencastDirector(object):
         self._append_command(_clear_marks, delay)
 
     def run_command(self, command, args=None):
-        def _run_command(cursor, e):
+        def _run_command(cursor, edit):
             self.target_view.sel().add(cursor)
             if args is None:
                 self.target_view.run_command(command)
@@ -312,6 +476,7 @@ class ScreencastDirectorBindSourceCommand(sublime_plugin.ApplicationCommand):
             source = source_view.substr(allofit)
             search = "\n\n-"
             parts = source.split(search)
+
             blocks = []
 
             skip = parts.pop(0)
@@ -328,6 +493,9 @@ class ScreencastDirectorBindSourceCommand(sublime_plugin.ApplicationCommand):
                 offset += len(part)
                 region = sublime.Region(start, end)
                 blocks.append(region)
+            if not blocks:
+                sublime.status_message('ScreencastDirector could not parse commands.')
+                return
             source_view.add_regions(
                 'screencast_director',
                 blocks,
@@ -368,6 +536,7 @@ class ScreencastDirectorRunCommand(sublime_plugin.ApplicationCommand):
         the_director._run()
         the_director.index += 1
         the_director._refresh_source()
+        sublime.status_message('Index is at {index}'.format(index=the_director.index))
 
 
 class ScreencastDirectorNextCommand(sublime_plugin.ApplicationCommand):
@@ -378,6 +547,7 @@ class ScreencastDirectorNextCommand(sublime_plugin.ApplicationCommand):
         else:
             the_director.index += 1
         the_director._refresh_source()
+        sublime.status_message('Index is at {index}'.format(index=the_director.index))
 
 
 class ScreencastDirectorPreviousCommand(sublime_plugin.ApplicationCommand):
@@ -388,3 +558,4 @@ class ScreencastDirectorPreviousCommand(sublime_plugin.ApplicationCommand):
         else:
             the_director.index -= 1
         the_director._refresh_source()
+        sublime.status_message('Index is at {index}'.format(index=the_director.index))
